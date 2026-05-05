4aab495ca4c9bd3c93ef8dcfad4a7a26c70781c3
Benjamin Luckow <bjluckow1@gmail.com>
2026-05-04 20:46:34 -0400

    comment out files route delim

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -23,7 +23,7 @@ export async function GET(req: Request) {
             Prefix: folder ? `${folder}/` : undefined,
             MaxKeys: pageSize,
             ContinuationToken: token,
-            Delimiter: "/",
+            // Delimiter: "/",
         }),
     );
 
